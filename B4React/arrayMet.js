39affc0542a0345ce88d3e712727c64c59746ed7
GicheruBenjamin <projectbuild201@gmail.com>
2024-04-22 23:35:39 +0300

    ArrayIterator methods

---
+++ B4React/arrayMet.js
@@ -0,0 +1,15 @@
+
+// An array containing ages of people in a work station
+let ages = [22, 25, 54, 60, 33, 55, 49, 29];
+
+// Map to find the ages each one was when the station started considering it began 4 years ago
+let modifiedAgesMap = ages.map(age => age - 4);
+console.log(modifiedAgesMap);
+
+// Reduce
+let totalAgesReduce = ages.reduce((total, age) => total + (age - 4), 0);
+console.log(totalAgesReduce);
+
+// Filter
+let filteredAges = ages.filter(age => age > 4);
+console.log(filteredAges);
--- B4React/desObj.js
+++ B4React/desObj.js
@@ -1,1 +1,9 @@
 //An object person 
+const person = {
+    name: 'John',
+    age: 34,
+    isMarried: true
+}
+
+const person2 = {...person, name: 'Jane'}
+console.log(person2);
